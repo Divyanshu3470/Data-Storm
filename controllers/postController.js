7733141f5c689267f4d73ef988158b7ca3246239
Divyanshu3470 <divyanshunegi1435@gmail.com>
2026-07-25 08:09:31 +0530

    Added PUT update route

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,20 +3,38 @@ import Post from "../models/Post.js";
 export const createPost = async (req, res) => {
   try {
     const post = await Post.create(req.body);
-
     res.status(201).json(post);
   } catch (err) {
-    res.json(err);
+    res.status(500).json(err);
   }
 };
 
 export const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
-
     res.json(posts);
   } catch (err) {
-    res.json(err);
+    res.status(500).json(err);
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    res.json(post);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
@@ -28,6 +46,6 @@ export const deletePost = async (req, res) => {
       message: "Post Deleted",
     });
   } catch (err) {
-    res.json(err);
+    res.status(500).json(err);
   }
 };
